5d1cb2fb856d211eb4e3484ef43b240b53b5134f
ARTALEAL <artaleal@yandex.ru>
2023-08-29 20:02:14 +0300

    swipers

---
+++ script/index.js
@@ -0,0 +1,23 @@
+const sliderMain = new Swiper('.slider_main', {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
+});
+
+const sliderBg = new Swiper('.slider_bg', {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 20,
+  slidesPerView: 3.5,
+});
